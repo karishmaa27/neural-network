324a70cd0ec41dce1c7e2880973824c7a16d6488
karishma <wadhwanikarishma15@gmail.com>
2024-07-22 23:06:45 +0530

    first commit

---
+++ xor.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+from dense import Dense
+from activations import Tanh
+from losses import mse, mse_prime
+from network import train, predict
+
+# Reshape input data appropriately
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+Y = np.array([[0], [1], [1], [0]])  
+
+network = [
+    Dense(2, 3),
+    Tanh(),
+    Dense(3, 1),
+    Tanh()
+]
+
+# train
+train(network, mse, mse_prime, X, Y, epochs=1000, learning_rate=0.1)
+
+# decision boundary plot
+points = []
+for x in np.linspace(0, 1, 20):
+    for y in np.linspace(0, 1, 20):
+        z = predict(network, [[x], [y]])
+        points.append([x, y, z[0,0]])
+
+points = np.array(points)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+ax.scatter(points[:, 0], points[:, 1], points[:, 2], c=points[:, 2], cmap="winter")
+plt.show()
